8582c7420644bdf977232ed5588229b94a57c9bd
alvcodec <148978672+alvcodec@users.noreply.github.com>
2023-12-21 22:03:57 +0000

    Refining and adding new JS features

--- Whac-a-mole/script.js
+++ Whac-a-mole/script.js
@@ -4,18 +4,17 @@ const timeLeft = document.querySelector('#timeLeft');
 const yourScore = document.querySelector('#score');
 
 let result = 0
-let hitPosition
+let hitPosition;
 let currentTime = 60;
-let timerId = null;
-
+let timerId;
+let countDownTimerId;
 function randomSquare() {
     squares.forEach(square => {
         square.classList.remove('mole', 'rise-shake');
-    })
+    });
 
     let randomSquare = squares[Math.floor(Math.random() * squares.length)];
     randomSquare.classList.add('mole', 'rise-shake');
-    
     hitPosition = randomSquare.id;
 
 }
@@ -26,25 +25,63 @@ squares.forEach(square => {
         if (square.id === hitPosition) {
             result++;
             yourScore.textContent = result;
-            // hitPosition.classList.add('rise-shake');
             hitPosition = null;
-
-
         }
         else {
             result--;
             yourScore.textContent = result;
             hitPosition = null;
         }
-   
+
     })
 })
 
-function moveMole() {
-    timerId = setInterval(randomSquare, 1000);
-}
+const levelEasy = document.getElementById('easy');
+const levelMedium = document.getElementById('medium');
+const levelHard = document.getElementById('hard');
+const startBtn = document.getElementById('startBtn');
+const resetBtn = document.getElementById('resetBtn');
 
+
+
+
+
+function moveMole() {
+    
+    let interval = 1000; //Default interval (easy) setting
+
+    levelEasy.addEventListener('click', () => {
+        interval = 1000;
+    });
+    levelMedium.addEventListener('click', () => {
+        interval = 750;
+    });
+    levelHard.addEventListener('click', () => {
+        interval = 500;
+    });
+
+    timerId = setInterval(randomSquare, interval);
+    
+    startBtn.addEventListener('click', startGame);
+    resetBtn.addEventListener('click', resetGame);
+
+    function startGame() {
+        countDownTimerId = setInterval(countDown, 1000);
+        result = 0;
+        yourScore.textContent = result;
+
+    }
+    function resetGame() {
+        clearInterval(countDownTimerId);
+        // clearInterval(timerId);
+        result = 0;
+        currentTime = 60;
+        yourScore.textContent = result;
+        timeLeft.textContent = currentTime;
+    }
+}
 moveMole();
+
 
 function countDown() {
     currentTime--;
@@ -53,11 +90,11 @@ function countDown() {
     if (currentTime === 0) {
         clearInterval(countDownTimerId);
         clearInterval(timerId);
-        alert('Game Over! Your score is'+ result );
+        alert('Game Over! Your score is' + result);
     }
+
 }
 
-let countDownTimerId = setInterval(countDown, 1000);
 
 // randomSquare();
 
